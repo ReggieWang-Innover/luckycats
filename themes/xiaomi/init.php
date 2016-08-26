effcacaecc691a2f11a8deee682ae98040721b00
reggie.wang <reggie.wang@3vr.com>
2016-08-26 12:33:55 +0800

    filter cats only if cat id is 0

--- themes/xiaomi/init.php
+++ themes/xiaomi/init.php
@@ -1389,7 +1389,10 @@ function get_cat_recommend_goods( $type = "", $cat_id = 0, $cat_num = 0, $brand 
 				$sql .= " AND is_promote = 1 AND promote_start_date <= '".$time."' AND promote_end_date >= '{$time}'";
 		}
 		
-		$sql .= " AND goods_type <> 1 ";
+		if ($cat_id == 0)
+		{
+		  $sql .= " AND goods_type <> 1 ";
+		}
 		
 		$cats = get_children( $cat_id );
 		if ( !empty( $cats ) )
